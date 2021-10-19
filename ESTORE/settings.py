b3c2ae92a31d71095f62515e13ece8f85b975f82
Subhan <79940729+SubhanRzayev@users.noreply.github.com>
2021-10-19 14:44:01 +0400

    Update settings.py

--- ESTORE/settings.py
+++ ESTORE/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6lgcu++_xii6-acfw@nk$8744s@ya5*j1pzhde&db51$%5xxn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
